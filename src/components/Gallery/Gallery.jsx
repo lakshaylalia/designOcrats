94d174f40243ecd2d93f52cdee0e4f23c71483a7
Lakshay Lalia <lakshaylalia@gmail.com>
2025-02-03 20:37:02 +0530

    Updated favicon

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -67,9 +67,9 @@ function Gallery() {
       {images.map((image) => (
         <motion.div
           key={image.id}
-          initial={{ opacity: 0, scale: 0.8 }}
+          initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 0.5, type: "spring" }}
           whileHover={{ scale: 1.1 }}
         >
           <img
